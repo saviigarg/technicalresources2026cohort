1cff857e1458c03e616a397a0e0fc561dd3ecca6
saviigarg <118602648+saviigarg@users.noreply.github.com>
2023-01-24 22:09:17 +0530

    submitting Q11 of A2

---
+++ a2_q11.py
@@ -0,0 +1,11 @@
+current_num=1
+rows=4
+stop=2
+for i in range(rows):
+   
+    for j in range(1,stop):
+        print(current_num, end='  ')
+        current_num=current_num+1
+    print("  ")
+    stop=stop+1
+        
